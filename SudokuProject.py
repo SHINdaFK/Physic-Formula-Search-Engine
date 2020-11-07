38207bcaccc4fee58911e84995d7d045d3a66ab6
vapourthecompsciguy <nguyennghiahiep2004@gmail.com>
2020-11-07 21:28:07 +0700

    hello

---
+++ SudokuProject.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+root= tk.Tk()
+
+
+import random
+
+[n1, n2, n3, n4, n5, n6, n7, n8, n9]
+[n1, n2, n3, n4, n5, n6, n7, n8, n9]
+[n1, n2, n3, n4, n5, n6, n7, n8, n9]
+[n1, n2, n3, n4, n5, n6, n7, n8, n9]
+[n1, n2, n3, n4, n5, n6, n7, n8, n9]
+[n1, n2, n3, n4, n5, n6, n7, n8, n9]
+[n1, n2, n3, n4, n5, n6, n7, n8, n9]
+[n1, n2, n3, n4, n5, n6, n7, n8, n9]
+[n1, n2, n3, n4, n5, n6, n7, n8, n9]
+[n1, n2, n3, n4, n5, n6, n7, n8, n9]
+
+
+
+
+number_list = list(range(1, 10))
+print(random.sample(number_list, 3))
