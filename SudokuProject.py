26cc1c8f8be24e7b288d6b74a2996328032b3716
vapourthecompsciguy <nguyennghiahiep2004@gmail.com>
2020-11-19 22:33:27 +0700

    hello

--- SudokuProject.py
+++ SudokuProject.py
@@ -6,18 +6,8 @@ import numpy as np
 root = Tk()
 root.geometry("800x600")
 
-class JIWOO:
-    grid = [
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,6],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0]
-    ]
+class JIWOO():
+    grid = retrieve_input
 
     def possible(y,x,n):
         global grid
@@ -51,15 +41,20 @@ class JIWOO:
 
     solve()
 
-class SudokuGUI:
+class SudokuGUI():
     def __init__(self, hello):
-        hello.title("Sudoku_Solver")
-        my_font = ("Airal", 18)
+
+        self.hello = hello
+        hello.title("SODUKU SOLVING MACHINE")
+        my_font = ("Times New Roman", 20)
+        px, py = 0, 0
+
+
         self.entry_box = [
         [0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,6],
+        [0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0],
@@ -67,10 +62,9 @@ class SudokuGUI:
         [0,0,0,0,0,0,0,0,0]
         ]
 
-        for i in range(9):
-            for j in range (9):
-                self.entry_box[i][j] = Entry(hello, font = my_font, bg = "white", highlightcolor = "yellow", borderwidth = 1, width = 2, cursor = 'star', highlightthickness = 1, highlightbackground = 'black', textvariable = retrieve_input[i][j])
-                self.entry_box[i][j].grid(row = [i], columnn = [j])
+        for i in range(0, 9):
+            for j in range (0, 9):
+                self.entry_box[i][j] = Entry(hello, font = my_font, bg = "white", highlightcolor = "yellow", bd = 1, width = 4, cursor = 'star', highlightthickness = 1, highlightbackground = 'black', textvariable = retrieve_input[i][j])
                 self.entry_box[i][j].bind('<Motion>', self.correctGrid)
                 self.entry_box[i][j].bind('<FocusIn>', self.correctGrid)
                 self.entry_box[i][j].bind('<Button-1>', self.correctGrid)
@@ -79,23 +73,26 @@ class SudokuGUI:
 
         menu = Menu(hello)
         file = Menu(menu)
-        file.add_command(hello, label = "Reset", command= self.restart_program)
-        file.add_command(hello, label = "Solve", command= self.solve_sudoku)
-        file.add_command(hello, label = "Quit the game", command = hello.quit)
-        menu.add_cascade(hello, label = "Menu", menu=file)
-        main.config(menu=menu)
+        file.add_command(label = "Reset", command= self.restart_program)
+        file.add_command(label = "Solve", command= self.solve_sudoku)
+        file.add_command(label = "Quit the game", command = hello.quit)
+        menu.add_cascade(label = "Menu", menu=file)
+        hello.config(menu=menu)
 
     def restart_program(self):
-        for x in range (0, 10):
-            for y in range (0, 10):
+        for x in range (9):
+            for y in range (9):
                 retrieve_input[i][j].set("")
+
+
+
     def correctGrid(self, event):
         for i in range(9):
             for j in range(9):
-                if savedNumbers[i][j].get() == '':
+                if retrieve_input[i][j].get() == '':
                     continue
-                if len(savedNumbers[i][j].get()) > 1 or savedNumbers[i][j].get() not in ['1','2','3','4','5','6','7','8','9']:
-                    savedNumbers[i][j].set('')
+                if len(retrieve_input[i][j].get()) > 1 or retrieve_input[i][j].get() not in ['1','2','3','4','5','6','7','8','9']:
+                    retrieve_input[i][j].set('')
 
 
     def solve_sudoku(self):
@@ -112,9 +109,9 @@ retrieve_input = [
         [0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0]
         ]
-for i in range (0, 10):
-    for j in range (0, 10):
-        retrieve_input = StringVar(root)
+for i in range (0, 9):
+    for j in range (0, 9):
+        retrieve_input[i][j] = StringVar(root)
 
 b = SudokuGUI(root)
 root.mainloop()
